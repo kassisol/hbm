4ba3bbde776469250f2a718dbf29034c18b1d566
Julien Kassar <github@kassisol.com>
2017-01-15 23:16:21 +0000

    Update server initialization

--- cmd/init.go
+++ cmd/init.go
@@ -1,16 +1,11 @@
 package cmd
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
-	"path"
 
 	"github.com/boltdb/bolt"
 	"github.com/kassisol/hbm/pkg/db"
-	"github.com/kassisol/hbm/pkg/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -23,21 +18,10 @@ var initCmd = &cobra.Command{
 func init() {
 	RootCmd.AddCommand(initCmd)
 
-	initCmd.Run = initialconfig
+	initCmd.Run = initConfig
 }
 
-func initialconfig(cmd *cobra.Command, args []string) {
-	var dockerPluginPath = "/etc/docker/plugins"
-	var dockerPluginFile = path.Join(dockerPluginPath, "hbm.spec")
-	var pluginSpecContent = []byte("unix://run/docker/plugins/hbm.sock")
-
-	_, err := exec.LookPath("docker")
-	if err != nil {
-		fmt.Println("Docker does not seem to be installed. Please check your installation.")
-
-		os.Exit(-1)
-	}
-
+func initConfig(cmd *cobra.Command, args []string) {
 	if _, err := os.Stat(appPath); os.IsNotExist(err) {
 		err := os.Mkdir(appPath, 0700)
 		if err != nil {
@@ -75,19 +59,5 @@ func initialconfig(cmd *cobra.Command, args []string) {
 	})
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	if _, err := os.Stat(dockerPluginPath); os.IsNotExist(err) {
-		err := os.Mkdir(dockerPluginPath, 0755)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	if !utils.FileExists(dockerPluginFile) {
-		err := ioutil.WriteFile(dockerPluginFile, pluginSpecContent, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
 	}
 }
--- cmd/server.go
+++ cmd/server.go
@@ -1,13 +1,18 @@
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"os/signal"
+	"path"
 	"syscall"
 
 	"github.com/docker/go-plugins-helpers/authorization"
 	"github.com/kassisol/hbm/plugin"
+	"github.com/kassisol/hbm/pkg/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -25,7 +30,38 @@ func init() {
 	serverCmd.Run = server
 }
 
+func serverInitConfig() {
+	var dockerPluginPath = "/etc/docker/plugins"
+	var dockerPluginFile = path.Join(dockerPluginPath, "hbm.spec")
+	var pluginSpecContent = []byte("unix://run/docker/plugins/hbm.sock")
+
+	_, err := exec.LookPath("docker")
+	if err != nil {
+		fmt.Println("Docker does not seem to be installed. Please check your installation.")
+
+		os.Exit(-1)
+	}
+
+	if _, err := os.Stat(dockerPluginPath); os.IsNotExist(err) {
+		err := os.Mkdir(dockerPluginPath, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if !utils.FileExists(dockerPluginFile) {
+		err := ioutil.WriteFile(dockerPluginFile, pluginSpecContent, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	log.Print("Server has completed initialization")
+}
+
 func server(cmd *cobra.Command, args []string) {
+	serverInitConfig()
+
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
 	signal.Notify(ch, syscall.SIGTERM)
@@ -42,5 +78,6 @@ func server(cmd *cobra.Command, args []string) {
 		log.Fatal(h.ServeUnix("root", "hbm"))
 	}()
 
-	<-ch
+	s := <-ch
+	log.Printf("Processing signal '%s'", s)
 }
