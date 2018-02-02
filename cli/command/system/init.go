3765ae9cc62345b8dcfcfbfa47aaff3319818b54
Julien Kassar <github@kassisol.com>
2018-02-02 16:00:19 -0500

    Add new param --action for init command

--- cli/command/system/init.go
+++ cli/command/system/init.go
@@ -3,10 +3,14 @@ package system
 import (
 	"github.com/juliengk/go-utils/filedir"
 	"github.com/kassisol/hbm/cli/command"
+	"github.com/kassisol/hbm/docker/endpoint"
+	resourceobj "github.com/kassisol/hbm/object/resource"
 	"github.com/kassisol/hbm/storage"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
+
+var initAction bool
 
 func NewInitCommand() *cobra.Command {
 	cmd := &cobra.Command{
@@ -16,6 +20,9 @@ func NewInitCommand() *cobra.Command {
 		Args:  cobra.NoArgs,
 		Run:   runInit,
 	}
+
+	flags := cmd.Flags()
+	flags.BoolVarP(&initAction, "action", "", false, "Initialize action resources")
 
 	return cmd
 }
@@ -36,6 +43,22 @@ func runInit(cmd *cobra.Command, args []string) {
 	if len(config) == 0 {
 		s.SetConfig("authorization", false)
 		s.SetConfig("default-allow-action-error", false)
+	}
+
+	if initAction {
+		r, err := resourceobj.New("sqlite", command.AppPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer r.End()
+
+		if r.Count("action") == 0 {
+			for _, u := range *endpoint.GetUris() {
+				if err := r.Add(u.Action, "action", u.Action, []string{}); err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
 	}
 }
 
